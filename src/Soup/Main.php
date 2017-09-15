582ac71213125c5b95834bf73c06fd9a37c14d0d
iNova <lynxuhc@gmail.com>
2017-09-14 17:56:17 -0800

    Add files via upload

--- src/Soup/Main.php
+++ src/Soup/Main.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Soup;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\Item;
+use pocketmine\block\Block;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class Main extends PluginBase implements Listener {
+
+  public function onEnable() {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function handleInteract(PlayerInteractEvent $event) { 
+  
+    if($event->getItem()->getId() == 282) {
+ 
+  $event->getPlayer()->sendMessage("§l§8- §eYou have consumed soup!");
+  $event->getPlayer()->setHealth($event->getPlayer()->getHealth() + 3);
+  $event->getPlayer()->getInventory()->setItemInHand(Item::get(0));
+  
+    }
+  }
+}
